0fdcebe96bf74f6ca33c25545634b3b5033e094e
SyedHasibRahman <syedhasib01@gmail.com>
2021-10-11 23:19:18 +0600

    install firebase & initialize Authentication

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB406lAl9T9BpZ8LH7_nOQy3sv_jECKsNc",
+    authDomain: "burj-al-arab-by-hasib.firebaseapp.com",
+    projectId: "burj-al-arab-by-hasib",
+    storageBucket: "burj-al-arab-by-hasib.appspot.com",
+    messagingSenderId: "436552927331",
+    appId: "1:436552927331:web:ec4c1e98069d9b55e7d056"
+};
+
+export default firebaseConfig;
